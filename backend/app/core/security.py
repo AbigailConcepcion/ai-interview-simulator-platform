9c07577e1076f680f82504c13775ca48f0cae56d
Abi Concepcion <concepcionabigail03@gmail.com>
2026-03-05 05:12:38 +0800

    Create security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,18 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from .config import SECRET_KEY
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def verify_password(password, hashed):
+    return pwd_context.verify(password, hashed)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
